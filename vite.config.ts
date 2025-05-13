2c9663a89fb8976bc1815746fa0c9f9b32fc5914
hseaiyman <hseaiymanahmad@gmail.com>
2025-05-14 00:14:14 +0300

    Fix Netlify build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -15,35 +15,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    copyPublicDir: true,
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (!assetInfo.name) return 'assets/[name]-[hash][extname]';
-          
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-
-          if (/\.(png|jpe?g|gif|svg|webp|ico)$/i.test(ext)) {
-            return 'assets/images/[name]-[hash][extname]';
-          }
-          
-          if (/\.(pdf|doc|docx|xls|xlsx|txt)$/i.test(ext)) {
-            return 'assets/documents/[name]-[hash][extname]';
-          }
-          
-          if (/\.(woff2?|eot|ttf|otf)$/i.test(ext)) {
-            return 'assets/fonts/[name]-[hash][extname]';
-          }
-
-          return 'assets/[name]-[hash][extname]';
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-      },
-    },
-  },
+    copyPublicDir: true
+  }
 });
